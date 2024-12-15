a7871dd1fccedc08a3d6072a42e3d30401c3097c
Niklas L <niklas@demtrees.com>
2024-12-15 23:30:45 +0200

    Add initial starting screen to be worked on

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from 'react-router'
 import { RealtimeRoom } from './components/RealtimeRoom'
-import WindSound from './components/WindSound'
+import Main from './components/Main'
 
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <RealtimeRoom />
-      <WindSound />
+      <Routes>  
+        <Route path="/room" element={<RealtimeRoom />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router";
+import 'splitting/dist/splitting.css'
+import 'splitting/dist/splitting-cells.css'
+import Splitting from 'splitting'
+import { useEffect } from "react";
+import { createTimeline, stagger, animate, utils } from '@juliangarnierorg/anime-beta'
+
+export default function Main() {
+  useEffect(() => {
+    Splitting()
+
+    utils.$('.char').forEach(char => {
+      utils.set(char, { filter: 'blur(50px)', scale: 0, opacity: 0 })
+    })
+    // animate('.char', { filter: 'blur(0px)', opacity: 1, delay: stagger([200, 1000]) })
+
+    createTimeline({
+      defaults: {
+        duration: 1500,
+        ease: 'inOutQuad'
+      }
+     })
+     .add('.char', { filter: 'blur(0px)', opacity: 1, scale: 1 }, stagger(100))
+  }, [])
+
+  return (
+    <div className="container mx-auto max-w-3xl h-screen flex flex-col justify-center items-center">
+      <blockquote className="text-neutral-100 mb-4 font-serif text-3xl leading-none" data-splitting>
+        If you gaze long into an abyss, the abyss also gazes into you.
+        <cite className="text-neutral-500 text-base mt-4 block text-right">
+          - Friedrich Nietzsche
+        </cite>
+      </blockquote>
+
+      <Link to="/room" className="text-white hover:text-gray-300">Room</Link>
+    </div>
+  )
+}
+
