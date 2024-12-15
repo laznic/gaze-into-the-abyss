228b1eec61220d1663f818f6a1c3e0cee53c208a
Niklas L <niklas@demtrees.com>
2024-12-16 00:16:55 +0200

    Add some styling to the enter screen

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import 'splitting/dist/splitting.css'
 import 'splitting/dist/splitting-cells.css'
 import Splitting from 'splitting'
 import { useEffect } from "react";
-import { createTimeline, stagger, animate, utils } from '@juliangarnierorg/anime-beta'
+import { createTimeline, stagger, utils } from '@juliangarnierorg/anime-beta'
 
 export default function Main() {
   useEffect(() => {
@@ -12,7 +12,6 @@ export default function Main() {
     utils.$('.char').forEach(char => {
       utils.set(char, { filter: 'blur(50px)', scale: 0, opacity: 0 })
     })
-    // animate('.char', { filter: 'blur(0px)', opacity: 1, delay: stagger([200, 1000]) })
 
     createTimeline({
       defaults: {
@@ -24,16 +23,51 @@ export default function Main() {
   }, [])
 
   return (
-    <div className="container mx-auto max-w-3xl h-screen flex flex-col justify-center items-center">
-      <blockquote className="text-neutral-100 mb-4 font-serif text-3xl leading-none" data-splitting>
-        If you gaze long into an abyss, the abyss also gazes into you.
-        <cite className="text-neutral-500 text-base mt-4 block text-right">
-          - Friedrich Nietzsche
-        </cite>
-      </blockquote>
+    <>
+      <svg className="fixed inset-0 w-full h-full -z-10 ">
+        <defs>
+          <filter id="noise">
+            <feTurbulence 
+              id="turbFreq"
+              type="fractalNoise" 
+              baseFrequency="0.01"
+              seed="5"
+              numOctaves="1"
+            >
+            </feTurbulence>
+            <feGaussianBlur stdDeviation="10">
+              <animate
+                attributeName="stdDeviation"
+                values="10;50;10"
+                dur="20s"
+                repeatCount="indefinite"
+              />
+            </feGaussianBlur>
+            <feColorMatrix
+              type="matrix"
+              values="-1 0 0 0 -1
+                      0 -1 0 0 -1
+                      0 0 -1 0 -1
+                      0 0 0 25 -13"
+            />
+          </filter>
+        </defs>
+        <rect width="200%" height="200%" filter="url(#noise)" className="rotation-animation" />
+      </svg>
+      <div className="container mx-auto max-w-3xl h-screen flex flex-col justify-center items-center relative">
+        <blockquote className="text-neutral-100 mb-4 font-serif text-3xl leading-none z-10" data-splitting>
+          If you gaze long into an abyss, the abyss also gazes into you.
+          <cite className="text-neutral-500 text-base mt-4 block text-right">
+            - Friedrich Nietzsche
+          </cite>
+        </blockquote>
 
-      <Link to="/room" className="text-white hover:text-gray-300">Room</Link>
-    </div>
+
+        <Link to="/room" className="text-white hover:text-gray-300 z-10">Room</Link>
+      </div>
+      <div className="fixed inset-0 w-[95vw] h-[95vh] bg-black rounded-full blur-[128px] m-auto">
+      </div>
+    </>
   )
 }
 
