123a61cb5fc5ca8dfe4a533a125a5960c80807c3
Niklas L <niklas@demtrees.com>
2024-12-16 00:18:27 +0200

    Move stylized background to be visible on every route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,40 @@ import Main from './components/Main'
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      {/* Background Elements */}
+      <svg className="fixed inset-0 w-full h-full -z-10">
+        <defs>
+          <filter id="noise">
+            <feTurbulence 
+              id="turbFreq"
+              type="fractalNoise" 
+              baseFrequency="0.01"
+              seed="5"
+              numOctaves="1"
+            >
+            </feTurbulence>
+            <feGaussianBlur stdDeviation="10">
+              <animate
+                attributeName="stdDeviation"
+                values="10;50;10"
+                dur="20s"
+                repeatCount="indefinite"
+              />
+            </feGaussianBlur>
+            <feColorMatrix
+              type="matrix"
+              values="-1 0 0 0 -1
+                      0 -1 0 0 -1
+                      0 0 -1 0 -1
+                      0 0 0 25 -13"
+            />
+          </filter>
+        </defs>
+        <rect width="200%" height="200%" filter="url(#noise)" className="rotation-animation" />
+      </svg>
+      <div className="fixed inset-0 w-[95vw] h-[95vh] bg-black rounded-full blur-[128px] m-auto" />
+
+      {/* Routes */}
       <Routes>  
         <Route path="/room" element={<RealtimeRoom />} />
         <Route path="/" element={<Main />} />
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -23,51 +23,16 @@ export default function Main() {
   }, [])
 
   return (
-    <>
-      <svg className="fixed inset-0 w-full h-full -z-10 ">
-        <defs>
-          <filter id="noise">
-            <feTurbulence 
-              id="turbFreq"
-              type="fractalNoise" 
-              baseFrequency="0.01"
-              seed="5"
-              numOctaves="1"
-            >
-            </feTurbulence>
-            <feGaussianBlur stdDeviation="10">
-              <animate
-                attributeName="stdDeviation"
-                values="10;50;10"
-                dur="20s"
-                repeatCount="indefinite"
-              />
-            </feGaussianBlur>
-            <feColorMatrix
-              type="matrix"
-              values="-1 0 0 0 -1
-                      0 -1 0 0 -1
-                      0 0 -1 0 -1
-                      0 0 0 25 -13"
-            />
-          </filter>
-        </defs>
-        <rect width="200%" height="200%" filter="url(#noise)" className="rotation-animation" />
-      </svg>
-      <div className="container mx-auto max-w-3xl h-screen flex flex-col justify-center items-center relative">
-        <blockquote className="text-neutral-100 mb-4 font-serif text-3xl leading-none z-10" data-splitting>
-          If you gaze long into an abyss, the abyss also gazes into you.
-          <cite className="text-neutral-500 text-base mt-4 block text-right">
-            - Friedrich Nietzsche
-          </cite>
-        </blockquote>
+    <div className="container mx-auto max-w-3xl h-screen flex flex-col justify-center items-center relative">
+      <blockquote className="text-neutral-100 mb-4 font-serif text-3xl leading-none z-10" data-splitting>
+        If you gaze long into an abyss, the abyss also gazes into you.
+        <cite className="text-neutral-500 text-base mt-4 block text-right">
+          - Friedrich Nietzsche
+        </cite>
+      </blockquote>
 
-
-        <Link to="/room" className="text-white hover:text-gray-300 z-10">Room</Link>
-      </div>
-      <div className="fixed inset-0 w-[95vw] h-[95vh] bg-black rounded-full blur-[128px] m-auto">
-      </div>
-    </>
+      <Link to="/room" className="text-white hover:text-gray-300 z-10">Room</Link>
+    </div>
   )
 }
 
